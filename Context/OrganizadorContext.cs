dc92837176048123160d19b5fb878a50eb5a3db4
adrianocruzweb <adrianocruzweb@hotmail.com>
2024-09-16 13:43:41 -0300

    feat "ObeterPorId" done

--- Context/OrganizadorContext.cs
+++ Context/OrganizadorContext.cs
@@ -1,5 +1,10 @@
-using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using TrilhaApiDesafio.Models;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace TrilhaApiDesafio.Context
 {
